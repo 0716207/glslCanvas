cbecdd432b198eb2b40e33fd24b51003da09ff0c
xm3karen <55311079+karenluuuu3@users.noreply.github.com>
2022-01-27 20:31:31 +0800

    220127
    fade to switch pages

--- js/0602.js
+++ js/0602.js
@@ -33,3 +33,16 @@ $(document).ready(function(){
     });
 
 });
+
+
+// fade to another page
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
